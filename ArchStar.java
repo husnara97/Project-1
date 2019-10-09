5dd554787b1b47d395cfe744febc7369fbc425ca
Ubuntu <ubuntu@ip-172-31-55-26.ec2.internal>
2019-10-09 12:24:55 +0000

    ArchStar first draft

---
+++ ArchStar.java
@@ -0,0 +1,22 @@
+package ArchStar;
+public class ArchStar{
+	public int add (int x, int y)
+	{
+		return x+y;
+	}
+
+	public int multiply (int x, int y)
+	{
+		return x*y;
+	}
+	
+	public int subtract (int x, int y)
+	{
+		return x-y;
+	}
+
+	public int division (int x, int y)
+	{
+		return x/y;
+	}
+}
