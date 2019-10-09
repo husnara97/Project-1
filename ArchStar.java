5464d080af360d8a05fd4ef33a41f7d686900010
Ubuntu <ubuntu@ip-172-31-55-26.ec2.internal>
2019-10-09 12:38:17 +0000

    Updated file

--- ArchStar.java
+++ ArchStar.java
@@ -1,5 +1,10 @@
 package ArchStar;
 public class ArchStar{
+	public static void main(String args[])
+	{
+		System.out.println("Welcome to ArchStar");
+	}
+
 	public int add (int x, int y)
 	{
 		return x+y;
